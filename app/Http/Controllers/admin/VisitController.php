75c9d5bc40f7ec542769c45432421ffab9e4e750
ajil23 <azrilpraya@gmail.com>
2024-12-31 11:37:51 +0800

    feat: export visi recap

--- app/Http/Controllers/admin/VisitController.php
+++ app/Http/Controllers/admin/VisitController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers\admin;
 
+use App\Exports\SalesVisitExport;
 use App\Http\Controllers\Controller;
 use App\Models\DataSales;
 use App\Models\Sector;
 use App\Models\TransactionType;
 use App\Models\Visit;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class VisitController extends Controller
 {
@@ -18,5 +21,20 @@ class VisitController extends Controller
         $transaction = TransactionType::all();
         $sales = DataSales::all();
         return view('admin.visit.index', compact('visit', 'sector', 'transaction', 'sales'));
+    }
+
+    public function export($format)
+    {
+        if ($format == 'excel') {
+            return Excel::download(new SalesVisitExport, 'sales_visit.xlsx');
+        } elseif ($format == 'pdf') {
+            $visits = Visit::all();  // Ambil data yang ingin diekspor
+
+            // Render tampilan ke PDF
+            $pdf = Pdf::loadView('admin.visit.pdf', compact('visits'));
+
+            // Download PDF
+            return $pdf->download('sales_visit.pdf');
+        }
     }
 }
